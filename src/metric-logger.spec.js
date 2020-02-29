6bca209427bc4b3a5a9e522a51ee8153104a9f27
Miklós Antal <mr.antal@gmail.com>
2020-02-29 18:23:30 +0100

    migrate tests to jest and sinon fake timers

--- src/metric-logger.spec.js
+++ src/metric-logger.spec.js
@@ -1,27 +1,26 @@
 'use strict';
 
-const sinon = require('sinon');
-const chai = require('chai');
-chai.use(require('sinon-chai'));
-const expect = chai.expect;
-afterEach(() => sinon.restore());
-
 const metricLoggerFactory = require('./metric-logger');
 const Logger = require('@emartech/json-logger').Logger;
+const fakeTimers = require('@sinonjs/fake-timers');
 
+jest.spyOn(Logger.prototype, 'info');
 let metricLogger;
-beforeEach(() => (metricLogger = metricLoggerFactory()));
+let clock;
+beforeEach(() => {
+  jest.resetAllMocks();
+  metricLogger = metricLoggerFactory();
+  clock = fakeTimers.install();
+});
+afterEach(() => clock.uninstall());
 
 describe('metricLogger', () => {
   describe('count', () => {
     it('should log a single measurement at the next flush period', () => {
-      const clock = sinon.useFakeTimers();
-      sinon.stub(Logger.prototype, 'info');
-
       metricLogger.count('elfogyasztot-tap', 4);
 
       clock.tick(60 * 1000 + 1);
-      expect(Logger.prototype.info).to.have.been.calledWith('elfogyasztot-tap', {
+      expect(Logger.prototype.info).toBeCalledWith('elfogyasztot-tap', {
         min: 4,
         max: 4,
         count: 1,
@@ -31,22 +30,19 @@ describe('metricLogger', () => {
     });
 
     it('should aggregate measurements withing a flush period', () => {
-      const clock = sinon.useFakeTimers();
-      sinon.stub(Logger.prototype, 'info');
-
       metricLogger.count('elfogyasztot-tap', 4);
       metricLogger.count('elfogyasztot-tap', 8);
       metricLogger.count('kacsa-cucc', 18);
 
       clock.tick(60 * 1000 + 1);
-      expect(Logger.prototype.info).to.have.been.calledWith('elfogyasztot-tap', {
+      expect(Logger.prototype.info).toBeCalledWith('elfogyasztot-tap', {
         min: 4,
         max: 8,
         count: 2,
         sum: 12,
         avarage: 6
       });
-      expect(Logger.prototype.info).to.have.been.calledWith('kacsa-cucc', {
+      expect(Logger.prototype.info).toBeCalledWith('kacsa-cucc', {
         min: 18,
         max: 18,
         count: 1,
@@ -58,15 +54,12 @@ describe('metricLogger', () => {
 
   describe('start/stop', () => {
     it('should log min, max, count and sum when log was called only once', () => {
-      const clock = sinon.useFakeTimers();
-      sinon.stub(Logger.prototype, 'info');
-
       const measurementId = metricLogger.start('utlegeles');
       clock.tick(50);
       metricLogger.stop(measurementId);
 
       clock.tick(60 * 1000);
-      expect(Logger.prototype.info).to.have.been.calledWith('utlegeles', {
+      expect(Logger.prototype.info).toBeCalledWith('utlegeles', {
         min: 50,
         max: 50,
         count: 1,
@@ -76,9 +69,6 @@ describe('metricLogger', () => {
     });
 
     it('should aggregate metrics for two measurements and log after a timeout', () => {
-      const clock = sinon.useFakeTimers();
-      sinon.stub(Logger.prototype, 'info');
-
       const measurement1Id = metricLogger.start('utlegeles');
       const measurement2Id = metricLogger.start('utlegeles');
       clock.tick(50);
@@ -87,7 +77,7 @@ describe('metricLogger', () => {
       metricLogger.stop(measurement2Id);
 
       clock.tick(60 * 1000);
-      expect(Logger.prototype.info).to.have.been.calledWith('utlegeles', {
+      expect(Logger.prototype.info).toBeCalledWith('utlegeles', {
         min: 50,
         max: 70,
         sum: 120,
@@ -97,35 +87,27 @@ describe('metricLogger', () => {
     });
 
     it('should flush results at the closes minute:30', () => {
-      const clock = sinon.useFakeTimers(10 * 1000);
-      sinon.stub(Logger.prototype, 'info');
-
       const measurementId = metricLogger.start('utlegeles');
       metricLogger.stop(measurementId);
 
-      clock.tick(19 * 1000 + 999);
-      expect(Logger.prototype.info).not.to.have.been.called;
+      clock.tick(29 * 1000 + 999);
+      expect(Logger.prototype.info).not.toBeCalled();
       clock.tick(1 * 1000);
-      expect(Logger.prototype.info).to.have.been.calledWith('utlegeles');
+      expect(Logger.prototype.info).toBeCalledWith('utlegeles', expect.anything());
     });
 
     it('should flush at next minute:30 if we are after 30 in current minute', () => {
-      const clock = sinon.useFakeTimers(45 * 1000);
-      sinon.stub(Logger.prototype, 'info');
-
+      clock.tick(45 * 1000);
       const measurementId = metricLogger.start('utlegeles');
       metricLogger.stop(measurementId);
 
       clock.tick(44 * 1000);
-      expect(Logger.prototype.info).not.to.have.been.called;
+      expect(Logger.prototype.info).not.toBeCalled();
       clock.tick(1 * 1000);
-      expect(Logger.prototype.info).to.have.been.calledWith('utlegeles');
+      expect(Logger.prototype.info).toBeCalledWith('utlegeles', expect.anything());
     });
 
     it('should periodically log measuerments', () => {
-      const clock = sinon.useFakeTimers(0);
-      sinon.stub(Logger.prototype, 'info');
-
       const measurement1 = metricLogger.start('utlegeles');
       const measurement2 = metricLogger.start('utlegeles');
       clock.tick(10 * 1000);
@@ -133,7 +115,7 @@ describe('metricLogger', () => {
       metricLogger.stop(measurement1);
 
       clock.tick(21 * 1000);
-      expect(Logger.prototype.info).to.have.been.calledWith('utlegeles', {
+      expect(Logger.prototype.info).toBeCalledWith('utlegeles', {
         count: 1,
         max: 10000,
         min: 10000,
@@ -144,7 +126,7 @@ describe('metricLogger', () => {
       metricLogger.stop(measurement2);
       metricLogger.stop(measurement3);
       clock.tick(60 * 1000);
-      expect(Logger.prototype.info).to.have.been.calledWith('utlegeles', {
+      expect(Logger.prototype.info).toBeCalledWith('utlegeles', {
         count: 2,
         max: 31000,
         min: 21000,
@@ -154,9 +136,6 @@ describe('metricLogger', () => {
     });
 
     it('should log measurements for multiple tags', () => {
-      const clock = sinon.useFakeTimers(0);
-      sinon.stub(Logger.prototype, 'info');
-
       const measurement1 = metricLogger.start('utlegeles');
       const measurement2 = metricLogger.start('nyavogas');
       clock.tick(10 * 1000);
@@ -165,14 +144,14 @@ describe('metricLogger', () => {
       metricLogger.stop(measurement2);
       clock.tick(15 * 1000);
 
-      expect(Logger.prototype.info).to.have.been.calledWith('utlegeles', {
+      expect(Logger.prototype.info).toBeCalledWith('utlegeles', {
         count: 1,
         max: 10000,
         min: 10000,
         sum: 10000,
         avarage: 10000
       });
-      expect(Logger.prototype.info).to.have.been.calledWith('nyavogas', {
+      expect(Logger.prototype.info).toBeCalledWith('nyavogas', {
         count: 1,
         max: 15000,
         min: 15000,
