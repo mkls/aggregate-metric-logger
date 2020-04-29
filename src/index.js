e06f952ba051530c106a5054b15f0418fa099b6e
Miklós Antal <mr.antal@gmail.com>
2020-04-29 14:27:44 +0200

    SUITEDEV-21979: also expose a factory method to allow creating logger with custom namespace

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@
 
 const metricLoggerFactory = require('./metric-logger');
 
-module.exports = metricLoggerFactory({
-  enabled: process.env.METRIC_LOGGER_ENABLED === 'true',
+const enabled = process.env.METRIC_LOGGER_ENABLED === 'true';
+const exposedInstance = metricLoggerFactory({
+  enabled,
   namespace: process.env.METRIC_LOGGER_NAMESPACE || 'aggregate-metric-logger'
 });
+const exposedFactoryInstanceHybrid = namespace => metricLoggerFactory({ enabled, namespace });
+
+exposedFactoryInstanceHybrid.setThresholds = exposedInstance.setThresholds;
+exposedFactoryInstanceHybrid.measure = exposedInstance.measure;
+exposedFactoryInstanceHybrid.count = exposedInstance.count;
+exposedFactoryInstanceHybrid.trace = exposedInstance.trace;
+exposedFactoryInstanceHybrid.debug = exposedInstance.debug;
+exposedFactoryInstanceHybrid.debug = exposedInstance.info;
+exposedFactoryInstanceHybrid.debug = exposedInstance.warn;
+exposedFactoryInstanceHybrid.debug = exposedInstance.error;
+exposedFactoryInstanceHybrid.debug = exposedInstance.fatal;
+exposedFactoryInstanceHybrid.debug = exposedInstance.start;
+exposedFactoryInstanceHybrid.debug = exposedInstance.stop;
+
+module.exports = exposedFactoryInstanceHybrid;
